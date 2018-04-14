c97c7c115b56eb276a9c584ddccdf4fc588e3c93
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-14 17:32:07 -0500

    add libfii

---
+++ libfii_install_script.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export libfii_version=3.2.1
+
+name=libfii
+version=$libffii_version
+folder=$name-$version
+tar_f=$name-$version
+tarball=${name}-$version.tar.gz
+url=iftp://sourceware.org/pub/$name/$tarball
+
+mkdir $install_dir/$folder
+cd $install_dir
+ln -s $folder $name
+cd $install_dir/$folder
+
+wget $url
+tar -xvf $tarball
+mv $tar_f src
+
+setup_string=
+setup_string+=" "--prefix=$install_dir/$folder
+
+cd src 
+./configure $setup_string
+make -j $jobs
+make install
