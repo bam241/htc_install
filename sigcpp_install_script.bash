b43d6dffda73e1da494456122973c5fd0484a1fc
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-14 16:43:48 -0500

    fix sigcpp

--- sigcpp_install_script.bash
+++ sigcpp_install_script.bash
@@ -18,13 +18,13 @@ cd $install_dir/$folder
 
 wget $url
 tar -xvf $tarball
-mv $tar_f src
+mv lib$tar_f src
 mkdir bld
 
 setup_string=
 setup_string+=" "--prefix=$install_dir/$folder
 
-cd lib${folder_}
+cd src
 ./configure $setup_string
 make -j $jobs
 make install
