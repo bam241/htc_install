d588f26323954af5bb17df5c6cd9345199299c81
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-14 15:18:33 -0500

    add cbc

---
+++ cbc_install_script.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export Cbc_version=2.9.8
+
+name=Cbc
+version=$cbc_version
+folder=$name-$version
+tar_f=$name-$version
+tarball=${name}-$version.tgz
+url=http://www.coin-or.org/download/source/$name/$tarball
+
+mkdir $install_dir/$folder
+cd $install_dir
+ln -s $folder $name
+cd $install_dir/$folder
+
+wget $url
+tar -xvf $tarball
+mv $tar_f src
+mkdir bld
+
+setup_string=
+setup_string+=" "--prefix=$install_dir/$folder
+
+cd $name-$version
+./configure $setup_string
+make -j $jobs
+make install
+
