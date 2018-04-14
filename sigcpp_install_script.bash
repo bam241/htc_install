543331992c14a5d65b6162bd713e5ab564681e2c
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-14 15:58:08 -0500

    add sigcpp

---
+++ sigcpp_install_script.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export sigcpp_version=2.8.0
+
+name=sigcpp
+name_=sigc++
+version=$sigcpp_version
+folder=$name-$version
+folder_=$name_-$version
+tar_f=$name_-$version
+tarball=lib${name_}-$version.tar.xz
+url=https://download.gnome.org/sources/lib$name_/${version}/$tarball
+
+mkdir $install_dir/$folder
+cd $install_dir
+ln -s $folder $name
+cd $install_dir/$folder
+
+wget $url
+tar -xvf $tarball
+mv $tar_f src
+mkdir bld
+
+setup_string=
+setup_string+=" "--prefix=$install_dir/$folder
+
+cd lib${folder_}
+./configure $setup_string
+make -j $jobs
+make install
+
+
