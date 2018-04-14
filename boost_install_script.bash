218de7c7f136fecaf847a3266ed77082c0b05adb
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-14 14:24:43 -0500

    update

---
+++ boost_install_script.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+boost_version=1.61.0
+name=boost
+version=$boost_version
+folder=$name-$version
+tar_f=$name-$version
+
+tarball=${name}_$version
+tarball=`echo $tarball | sed s/'\.'/_/g`
+untar_f=$tarball
+tarball+=.tar.gz
+
+mkdir $install_dir/boost
+cd $install_dir/boost
+
+wget https://sourceforge.net/projects/boost/files/boost/$version/$tarball
+tar -xvf $tarball
+
+setup_string=
+setup_string+=" "--prefix=$install_dir/$folder
+setup_string+=" "--with-libraries=program_options,filesystem,system,serialization,regex
+
+cd ${tarball:0:12}
+./bootstrap.sh  --show-libraries
+./bootstrap.sh $setup_string
+./b2 install
+
---
+++ python_install_script.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+python_version=2.7.11
+
+name=python
+version=$python_version
+folder=$name-$version
+tarball=Python-$version.tgz
+tar_f=Python-$version
+url=https://www.python.org/ftp/python/$version/$tarball
+
+mkdir $install_dir/$folder
+cd $install_dir
+ln -s $folder $name
+cd $install_dir/$folder
+
+wget $url
+tar -xvf $tarball
+mv $tar_f src
+
+config_string=
+config_string+=" "--enable-shared
+config_string+=" "--prefix=$install_dir/$folder
+
+cd bld
+../src/configure $config_string
+make -j $jobs
+make install
+
