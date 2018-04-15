2dc47efa314caf66c598e578288907821717a63e
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-15 17:15:11 -0500

    cleaning pcre

--- pcre_install_script.bash
+++ pcre_install_script.bash
@@ -9,6 +9,8 @@ tar_f=$name-$version
 tarball=${name}-$version.tar.gz
 url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/$name/$tarball
 
+rm -rf $install_dir/$folder
+rm -rf $install_dir/$name
 mkdir $install_dir/$folder
 cd $install_dir
 ln -s $folder $name
