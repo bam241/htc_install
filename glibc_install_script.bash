c6f3309da599cf33887b8fac79dd88da4bf8a394
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-15 17:49:09 -0500

    adding glibc

---
+++ glibc_install_script.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export glibc_version=2.14
+
+name=glibc
+version=$glibc_version
+folder=$name-$version
+tar_f=$name-$version
+tarball=${name}-$version.tar.xz
+url=http://ftp.gnu.org/gnu/$name/$tarball
+
+rm -rf $install_dir/$folder
+rm -rf $install_dir/$name
+mkdir $install_dir/$folder
+cd $install_dir
+ln -s $folder $name
+cd $install_dir/$folder
+
+wget $url
+tar -xvf $tarball
+mv $tar_f src
+
+unset LD_LIBRARY_PATH
+
+setup_string=
+setup_string+=" "--prefix=$install_dir/$folder
+
+cd $name-$version
+mkdir autoconf
+cd autoconf
+../configure $setup_string
+make -j $jobs
+make install
