3c39d6d3516087b82a1b065169585f7e34bc4773
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-15 17:25:28 -0500

    cleaning glibmm

--- glibmm_install_script.bash
+++ glibmm_install_script.bash
@@ -10,6 +10,7 @@ tarball=${name}-$version.tar.xz
 url=http://ftp.gnome.org/pub/GNOME/sources/$name/${version: : ${#version}-2}/$tarball
 
 rm -rf $install_dir/$folder
+rm -rf $install_dir/$name
 mkdir $install_dir/$folder
 cd $install_dir
 ln -s $folder $name
