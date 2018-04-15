d5ccea43431311f2a9eadd4053dca8aa924eccbf
Mouginot B <mouginot.baptiste@gmail.com>
2018-04-15 10:17:24 -0500

    add rerun

---
+++ rerun.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./python_install_script.bash
+./mpc_install_script.bash
+./mpfr_install_script.bash
+./gmp_install_script.bash
+./boost_install_script.bash
+./Cbc_install_script.bash
+./sigcpp_install_script.bash
+./sqlite_install_script.bash
+./libfi_install_script.bash
+./glib_install_script.bash
+
